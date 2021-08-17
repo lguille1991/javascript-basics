0692c57f33f02bccaba1766afa241750c7a67a77
Guillermo Abrego <lguille1991@gmail.com>
2021-08-17 08:50:20 -0600

    Initial commit

---
+++ functions.js
@@ -0,0 +1,14 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(10, 10)); // 20
+
+function product(a, b) {
+  return a * b;
+}
+
+console.log(product(12, 12)); // 144
+
+let result = product(10, 10);
+console.log(result); // 100
